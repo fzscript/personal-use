9f5363de1efeddd50348274dab2d8c5ec8af5b70
fzscript <139060278+fzscript@users.noreply.github.com>
2023-07-18 14:27:09 +0700

    Update install-crypt.sh

--- install-crypt.sh
+++ install-crypt.sh
@@ -3,8 +3,8 @@ pkg install nodejs
 clear
 npm install -g bash-obfuscate
 echo -e "\e[0;33m"
-wget https://gitlab.com/pembriahmad/resources/-/raw/master/Bash-Crypt/crypt.py
+wget https://raw.githubusercontent.com/fzscript/personal-use/fz-tools/crypt.py
 rm install-crypt.sh
-echo -e "\e[0;33mSuccess Installations..."
-echo -e "\e[0;33mRun command type \e[4;31mpython2 crypt.py\e[0m"
+echo -e "\e[0;32mInstalasi berhasil..."
+echo -e "\e[0;32mJalankan perintah ini \e[5;33mpython2 crypt.py\e[0m"
 echo
